c057202a9f06f789d294fab80da09b3fb1d841f4
ToniTang <admin@realmsofpvp.net>
2014-01-13 17:11:18 -0600

    Added basic skeleton for pneumatics command

---
+++ src/edu/wpi/first/wpilibj/templates/commands/CommandPneumatics.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.commands;
+
+/**
+ *
+ * @author Anthony
+ */
+public class CommandPneumatics extends CommandBase {
+    
+    public CommandPneumatics() {
+        requires(pneumatics);
+    }
+
+    protected void initialize() {
+        throw new java.lang.UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected void execute() {
+        throw new java.lang.UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected boolean isFinished() {
+        throw new java.lang.UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected void end() {
+        throw new java.lang.UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    protected void interrupted() {
+        throw new java.lang.UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Pneumatics.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Pneumatics extends Subsystem {
+
+    public Pneumatics() {
+    
+    }
+    
+    protected void initDefaultCommand() {
+        setDefaultCommand();
+    }
+    
+}
