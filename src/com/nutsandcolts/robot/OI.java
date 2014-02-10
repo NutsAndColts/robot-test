6ad9890eb1e8465950a67148059b45fc34e24b8c
Zach Kimberg <zachary@kimberg.com>
2014-02-10 15:35:26 -0600

    Jaguar tester branch

--- src/com/nutsandcolts/robot/OI.java
+++ src/com/nutsandcolts/robot/OI.java
@@ -1,17 +1,18 @@
 package com.nutsandcolts.robot;
 
-import com.nutsandcolts.robot.commands.IntakeIdle;
-import com.nutsandcolts.robot.commands.IntakePull;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-
 import com.nutsandcolts.commands.pneumatics.IdleCompressor;
 import com.nutsandcolts.commands.pneumatics.RunCompressor;
 import com.nutsandcolts.commands.pneumatics.LiftInTake;
 import com.nutsandcolts.commands.pneumatics.LowerInTake;
 import com.nutsandcolts.commands.pneumatics.StopLift;
 import com.nutsandcolts.robot.commands.IntakePush;
+import com.nutsandcolts.robot.commands.IntakeIdle;
+import com.nutsandcolts.robot.commands.IntakePull;
+import com.nutsandcolts.robot.commands.FullJaguar;
+import com.nutsandcolts.robot.commands.IdleJaguar;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
@@ -19,40 +20,10 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-    //// CREATING BUTTONS
-    // One type of button is a joystick button which is any button on a joystick.
-    // You create one by telling it which joystick it's on and which button
-    // number it is.
-    // Joystick stick = new Joystick(port);
-    // Button button = new JoystickButton(stick, buttonNumber);
-    
-    // Another type of button you can create is a DigitalIOButton, which is
-    // a button or switch hooked up to the cypress module. These are useful if
-    // you want to build a customized operator interface.
-    // Button button = new DigitalIOButton(1);
-    
-    // There are a few additional built in buttons you can use. Additionally,
-    // by subclassing Button you can create custom triggers and bind those to
-    // commands the same as any other Button.
-    
-    //// TRIGGERING COMMANDS WITH BUTTONS
-    // Once you have a button, it's trivial to bind it to a button in one of
-    // three ways:
-    
-    // Start the command when the button is pressed and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenPressed(new ExampleCommand());
-    
-    // Run the command while the button is being held down and interrupt it once
-    // the button is released.
-    // button.whileHeld(new ExampleCommand());
-    
-    // Start the command when the button is released  and let it run the command
-    // until it is finished as determined by it's isFinished method.
-    // button.whenReleased(new ExampleCommand());
     
     public static final int JOYSTICK_PORT = 1;
     private Joystick stick = new Joystick(JOYSTICK_PORT);
+    
     Button b1 = new JoystickButton(stick,1);
     Button b2 = new JoystickButton(stick,2);
     Button b3 = new JoystickButton(stick,3);
@@ -61,6 +32,9 @@ public class OI {
     Button b6 = new JoystickButton(stick,6);
     Button b7 = new JoystickButton(stick,7);
     Button b8 = new JoystickButton(stick,8);
+    
+    Button fullJag = new JoystickButton(stick,9);
+    Button idleJag = new JoystickButton(stick,10);
     
     public OI() {
         b1.whenPressed(new IntakePull());
@@ -73,6 +47,9 @@ public class OI {
         
         b7.whenPressed(new RunCompressor());
         b8.whenReleased(new IdleCompressor());
+        
+        fullJag.whileHeld(new FullJaguar());
+        idleJag.whileHeld(new IdleJaguar());
     }
     
     public Joystick getJoystick() {
---
+++ src/com/nutsandcolts/robot/subsystems/JagTester.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nutsandcolts.robot.subsystems;
+
+import com.nutsandcolts.robot.RobotMap;
+import com.nutsandcolts.robot.commands.ZJaguar;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author Zach Kimberg
+ */
+public class JagTester extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    Jaguar jag = new Jaguar(RobotMap.testJaguar);
+    
+    public void set(double value){
+        jag.set(value);
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new ZJaguar());
+    }
+}
