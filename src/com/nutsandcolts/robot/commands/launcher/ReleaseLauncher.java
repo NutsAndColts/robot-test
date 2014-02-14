fa60683e0e471acf1a4b00bb07d74b8fa29c2c4d
Zach Kimberg <zachary@kimberg.com>
2014-02-14 15:31:19 -0600

    Code now works (for no apparent reason)

--- src/com/nutsandcolts/robot/commands/launcher/ReleaseLauncher.java
+++ src/com/nutsandcolts/robot/commands/launcher/ReleaseLauncher.java
@@ -19,7 +19,7 @@ public class ReleaseLauncher extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-//        launcher.disable();
+        launcher.disable();
         launcher.releaseMotor();
     }
 
--- src/com/nutsandcolts/robot/subsystems/Launcher.java
+++ src/com/nutsandcolts/robot/subsystems/Launcher.java
@@ -10,38 +10,38 @@ import com.nutsandcolts.robot.RobotMap;
 import com.nutsandcolts.robot.commands.launcher.ReleaseLauncher;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.Jaguar;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
  *
  * @author Anthony
  */
-public class Launcher extends Subsystem {
+public class Launcher extends PIDSubsystem {
 
-//    private static final double Kp = 1.0;
-//    private static final double Ki = 0.0;
-//    private static final double Kd = 0.0;
+    private static final double Kp = 1.0;
+    private static final double Ki = 0.0;
+    private static final double Kd = 0.0;
     
     private Jaguar motor = new Jaguar(RobotMap.launcherMotor);
     private Gyro gyro = new Gyro(RobotMap.gyro);
     
-    /* public Launcher() {
+    public Launcher() {
         super("Launcher", Kp, Ki, Kd);
-    } */
+    }
     
     public void initDefaultCommand() {
         setDefaultCommand(new ReleaseLauncher());
     }
     
-//    protected double returnPIDInput() {
-//        Preseason2014.disp(2, "Gyro angle: " + gyro.getAngle());
-//        Preseason2014.disp(3, "Gyro pidGet: " + gyro.pidGet());
-//        return gyro.pidGet();
-//    }
+    protected double returnPIDInput() {
+        Preseason2014.disp(2, "Gyro angle: " + gyro.getAngle());
+        Preseason2014.disp(3, "Gyro pidGet: " + gyro.pidGet());
+        return gyro.pidGet();
+    }
     
-//    protected void usePIDOutput(double output) {
-//        motor.pidWrite(output);
-//    }
+    protected void usePIDOutput(double output) {
+        motor.pidWrite(output);
+    }
     
     public void releaseMotor() {
         motor.set(0);
